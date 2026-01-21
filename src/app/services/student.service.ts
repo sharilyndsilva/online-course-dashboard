36ac0c5917f52816ea5b0ead5d929c09314cbb71
Sharilyn <sharilyn.dsilva@gmail.com>
2026-01-21 10:53:18 +0530

    Add StudentService with mock data

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Student } from '../models/student';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Student {
-  
+export class StudentService {
+
+  private students: Student[] = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john@example.com'
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      email: 'jane@example.com'
+    }
+  ];
+
+  getStudents(): Student[] {
+    return this.students;
+  }
 }
