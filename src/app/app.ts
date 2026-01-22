ceeb86d6dad6a409df0f2746c9affbdc9520a009
Sharilyn <sharilyn.dsilva@gmail.com>
2026-01-22 14:58:39 +0530

    Add navbar with routing navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
 })
 export class AppComponent {}
